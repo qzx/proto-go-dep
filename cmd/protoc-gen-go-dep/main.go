6ff000f61dd4ad707d4aacd2a65ed545a4c228cb
Freyr Guðjónsson <freyr.gudjonsson@crayon.com>
2023-09-12 16:50:21 +0000

    Some database model stuff

--- cmd/protoc-gen-go-dep/main.go
+++ cmd/protoc-gen-go-dep/main.go
@@ -54,8 +54,19 @@ func (p *Generator) Generate() (*pluginpb.CodeGeneratorResponse, error) {
 
 
         g.P("package ", protoFile.GoPackageName)
+        g.P("import (")
+        g.P(`   "database/sql"`)
+        g.P(`   _ "github.com/lib/pq"`)
+        g.P(")")
+        g.P("")
 
         for _, message := range protoFile.Messages {
+            p.generateModel(g, message)
+            p.generateListFunction(g, message)
+            p.generateCreateFunction(g, message)
+            p.generateUpdateFunction(g, message)
+            p.generateDeleteFunction(g, message)
+            p.generateFormHandler(g, message)
             p.generateDepsFunction(g, message)
         }
 
@@ -64,15 +75,145 @@ func (p *Generator) Generate() (*pluginpb.CodeGeneratorResponse, error) {
     return p.plugin.Response(), nil
 }
 
+func (p *Generator) generateModel(g *protogen.GeneratedFile, message *protogen.Message) {
+    typeName := string(message.Desc.Name())
+
+    g.P("// Lets start by creating a Model and Handler for our flow")
+    g.P("type Handler struct {")
+    g.P("   m *model")
+    g.P("}")
+    g.P("")
+    g.P("type model struct {")
+    g.P("   DB *sql.DB")
+    g.P("   Table string")
+    g.P("}")
+    g.P("")
+    g.P("func NewHandler(db *sql.DB) *Handler {")
+    g.P("   model := New(model)")
+    g.P("   model.DB = db")
+    g.P(`   model.Table = "`, strings.ToLower(typeName), `"`) 
+    g.P("}")
+    g.P("")
+}
+
+func (p *Generator) generateListFunction(g *protogen.GeneratedFile, message *protogen.Message) {
+    typeName := string(message.Desc.Name())
+
+    g.P("// List function should return a list of these objects")
+    g.P("func (m *model) List(tenant string) (map[int]", typeName, ", error) {")
+    g.P("   ret := make(map[int]", typeName, ")")
+    g.P("")
+    g.P(`   rows, err := m.DB.Query("SELECT id, data FROM list_data($1, $2)", tenant, m.Table)`)
+    g.P("   if err != nil { return ret, err }")
+    g.P("")
+    g.P("   defer rows.Close()")
+    g.P("")
+    g.P("   for rows.Next() {")
+    g.P("       var row ", typeName)
+    g.P("       var id int")
+    g.P("")
+    g.P("       err := rows.Scan(&id, &row)")
+    g.P("       if err != nil { return ret, err }")
+    g.P("")
+    g.P("       ret[id] = row")
+    g.P("   }")
+    g.P("")
+    g.P("   return ret, nil")
+    g.P("}")
+    g.P("")
+}
+
+func (p *Generator) generateGetFunction(g *protogen.GeneratedFile, message *protogen.Message) {
+    typeName := string(message.Desc.Name())
+
+    g.P("// Get function acquires a single record based on ID in database")
+    g.P(`func (m *model) Get(tenant string, id int) (*`, typeName, `, error) {`)
+    g.P("   ret := new(", typeName, ")")
+    g.P("")
+    g.P(`   err := m.DB.QueryRow("SELECT data FROM list_data($1, $2) WHERE id = $3",`)
+    g.P("       tenant, m.Table, id).Scan(&contact)")
+    g.P("")
+    g.P("   if err != nil { return nil, err }")
+    g.P("")
+    g.P("   return ret, nil")
+    g.P("}")
+    g.P("")
+}
+
+func (p *Generator) generateCreateFunction(g *protogen.GeneratedFile, message *protogen.Message) {
+    typeName := string(message.Desc.Name())
+    var firstField string
+    if len(message.Fields) > 0 {
+        firstField = message.Fields[0].GoName
+    } else {
+        return
+    }
+
+    checkField := strings.Join([]string{typeName, firstField}, ".")
+    
+    g.P("// Create function will create a new object of this type")
+    g.P(`func (m *model) Create(tenant string, data *`, typeName, `) error {`)
+    g.P("   if ", checkField, ` != "" {`)
+    g.P(`       _, err := m.DB.Exec("CALL insert_data($1, $2, $3)", tenant, m.Table, contact)`)
+    g.P("")
+    g.P("       if err != nil { return err }")
+    g.P("   } else {")
+    g.P(`       return errors.New("`, firstField, ` Was not set")`)
+    g.P("   }")
+    g.P("")
+    g.P("   return nil")
+    g.P("}")
+    g.P("")
+}
+
+
+func (p *Generator) generateUpdateFunction(g *protogen.GeneratedFile, message *protogen.Message) {
+    typeName := string(message.Desc.Name())
+
+    g.P("// Update function will replace the object stored at the given ID")
+    g.P(`func (m *model) Update(tenant string, id string, data *`, typeName, `) error {`)
+    g.P(`   _, err := m.DB.Exec("CALL update_data($1, $2, $3, $4)",`)
+    g.P("       tenant, m.Table, id, data)")
+    g.P("")
+    g.P("   return err")
+    g.P("}")
+    g.P("")
+}
+
+func (p *Generator) generateDeleteFunction(g *protogen.GeneratedFile, message *protogen.Message) {
+    g.P("// Delete function will... well delete the object at given ID")
+    g.P(`func (m *model) Delete(tenant string, id string) error {`)
+    g.P(`   _, err := m.DB.Exec("CALL delete_data_by_id($1, $2, $3)",`)
+    g.P("       tenant, m.Table, id)")
+    g.P("")
+    g.P("   return err")
+    g.P("}")
+    g.P("")
+}
+
+func (p *Generator) generateFormHandler(g *protogen.GeneratedFile, message *protogen.Message) {
+    typeName := string(message.Desc.Name())
+
+    g.P("// A simple function to handle a htmx form and populate the struct")
+    g.P(`func (x *`, typeName, `) HandleForm(req *http.Request) error {`)
+    if len(message.Fields) > 0 {
+        for _, field := range message.Fields {
+            formField := strings.Join([]string{typeName, field.GoName}, "__")
+            g.P(`       x.`, field.GoName, ` = req.FormValue("`, formField, `")`)
+        }
+    }
+    g.P("   return x.Validate()")
+    g.P("}")
+}
+
 func (p *Generator) generateDepsFunction(g *protogen.GeneratedFile, message *protogen.Message) {
 	typeName := string(message.Desc.Name())
-	msgName := string(message.Desc.Name())
 
     g.P(`// Deps function returns a static string for the time being, needs dev`)
-	g.P(`func (t *`, typeName, `) Deps  () string {`)
-
-    g.P(`return "`, msgName, `"`)
+	g.P(`func (t *`, typeName, `) Deps() string {`)
+    g.P(`return "`, typeName, `"`)
 	g.P(`}`)
+    g.P("")
 }
 
 func main() {
@@ -105,16 +246,6 @@ func main() {
     }
 
     fmt.Fprint(os.Stdout, string(out))
-
-/*	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
-		for _, f := range gen.Files {
-			if !f.Generate {
-				continue
-			}
-			generateFile(gen, f)
-		}
-		return nil
-	})*/
 }
 
 func parseParameter(param string) map[string]string {
@@ -132,20 +263,3 @@ func parseParameter(param string) map[string]string {
 
 	return paramMap
 }
-
-// generateFile generates a _dep.pb.go file containing gRPC service definitions.
-/*func generateFile(gen *protogen.Plugin, file *protogen.File) {
-	filename := file.GeneratedFilenamePrefix + "_dep.pb.go"
-    t := template.Must(template.New("dep").Parse(tmpl))
-	g := gen.NewGeneratedFile(filename, file.GoImportPath)
-    var buf bytes.Buffer
-    t.Execute(&buf, gen)
-
-	g.P("// Code generated by protoc-gen-go-dep. DO NOT EDIT.")
-	g.P()
-	g.P("package ", file.GoPackageName)
-	g.P("import ", "\"fmt\"")
-    log.Println(buf.String())
-
-//	return g
-}*/
--- example/example.pb.dep.go
+++ example/example.pb.dep.go
@@ -1,5 +1,90 @@
 package example
 
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+// Lets start by creating a Model and Handler for our flow
+type Handler struct {
+	m *model
+}
+
+type model struct {
+	DB    *sql.DB
+	Table string
+}
+
+func NewHandler(db *sql.DB) *Handler {
+	model := New(model)
+	model.DB = db
+	model.Table = "hello"
+}
+
+// List function should return a list of these objects
+func (m *model) List(tenant string) (map[int]Hello, error) {
+	ret := make(map[int]Hello)
+
+	rows, err := m.DB.Query("SELECT id, data FROM list_data($1, $2)", tenant, m.Table)
+	if err != nil {
+		return ret, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var row Hello
+		var id int
+
+		err := rows.Scan(&id, &row)
+		if err != nil {
+			return ret, err
+		}
+
+		ret[id] = row
+	}
+
+	return ret, nil
+}
+
+// Create function will create a new object of this type
+func (m *model) Create(tenant string, data *Hello) error {
+	if Hello.Email != "" {
+		_, err := m.DB.Exec("CALL insert_data($1, $2, $3)", tenant, m.Table, contact)
+
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("Email Was not set")
+	}
+
+	return nil
+}
+
+// Update function will replace the object stored at the given ID
+func (m *model) Update(tenant string, id string, data *Hello) error {
+	_, err := m.DB.Exec("CALL update_data($1, $2, $3, $4)",
+		tenant, m.Table, id, data)
+
+	return err
+}
+
+// Delete function will... well delete the object at given ID
+func (m *model) Delete(tenant string, id string) error {
+	_, err := m.DB.Exec("CALL delete_data_by_id($1, $2, $3)",
+		tenant, m.Table, id)
+
+	return err
+}
+
+// A simple function to handle a htmx form and populate the struct
+func (x *Hello) HandleForm(req *http.Request) error {
+	x.Email = req.FormValue("Hello__Email")
+	x.Name = req.FormValue("Hello__Name")
+	return x.Validate()
+}
+
 // Deps function returns a static string for the time being, needs dev
 func (t *Hello) Deps() string {
 	return "Hello"
